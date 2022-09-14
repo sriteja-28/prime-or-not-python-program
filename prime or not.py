550d5c401acc97bc7c102e71125eca14d7f8633a
MUTHANGI SRITEJA <108087209+sriteja-28@users.noreply.github.com>
2022-09-14 19:05:58 +0530

    Add files via upload
    prime or not using flag

--- prime or not.py
+++ prime or not.py
@@ -1,11 +1,12 @@
 num = int(input("Enter a number: "))
+flag = False
 if num > 1:
-    for i in range(2,int(num/2)+1):
+    for i in range(2, num):
         if (num % i) == 0:
-            print(num,"not a prime")
+            flag = True
             break
-        else:
-            print(num, "is a prime number")
+if flag:
+    print(num, "is not a prime number")
 else:
-    print(num, "is not prime number")
-   
+    print(num, "is a prime number")
+
